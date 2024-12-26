152a72c2f3daee35d7f899a0ad9f97e83d796431
emerson <emersoneletrotecnico2013@hotmail.com>
2024-12-25 21:17:51 -0300

    models
    criando os modelos das orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Address(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.city}"
+
+class Order(models.Model):
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='orders')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name}"
+    
+    def get_cost(self):
+        return self.price * self.quantity
