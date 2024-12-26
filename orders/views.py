dffbe5fa635bde1b0e592c946ff05686451055b7
emerson <emersoneletrotecnico2013@hotmail.com>
2024-12-25 21:20:22 -0300

    views
    criando a views para checkpot

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order, OrderItem, Address
+from .forms import AddressForm
+from cart.cart import Cart
 
-# Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            address = form.save()
+            order = Order.objects.create(address=address)
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            # Limpar o carrinho após o pedido
+            cart.clear()
+            return redirect('orders:order_created', order_id=order.id)
+    else:
+        form = AddressForm()
+    return render(request, 'orders/order_create.html', {'cart': cart, 'form': form})
+
+def order_created(request, order_id):
+    order = Order.objects.get(id=order_id)
+    return render(request, 'orders/order_created.html', {'order': order})
