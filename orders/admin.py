7b0138c47a8eab1bd45b702f6c06c3e9d55ffb78
emerson <emersoneletrotecnico2013@hotmail.com>
2024-12-25 21:18:55 -0300

    admin
    configurando o admin de orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Address, Order, OrderItem
 
-# Register your models here.
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'city', 'created')
+    list_filter = ('created',)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'address', 'created', 'updated', 'paid')
+    list_filter = ('paid', 'created', 'updated')
+    inlines = [OrderItemInline]
+
